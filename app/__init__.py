87130b36fb418615f143818bbce2903ba40b6874
Abhi <suriabhinav1997@gmail.com>
2017-09-24 02:59:10 -0400

    preboot

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,10 @@
 from flask import Flask
+from .config import config
 from flask_sqlalchemy import SQLAlchemy
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-# configuration stuff
-app.config['SECRET_KEY'] = 'super secret key'
-
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-        os.path.join(basedir, 'app.db')
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(app.config.from_object(config['development']))
 
 db = SQLAlchemy(app)
 
-from app import models
+from app import views, models
---
+++ app/config.py
@@ -0,0 +1,31 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super secret key'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    WTF_CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    WTF_CSRF_ENABLED = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
